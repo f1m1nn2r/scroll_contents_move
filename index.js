4a022260cff1c177009c0dc9d7c9b839cd417dc1
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-04-26 16:36:25 +0900

    2023.04.26_16:36

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 
-var sectionInner_fir = document.querySelector('.section-inner01');
-var sectionInner_sec = document.querySelector('.section-inner02');
-var pp = document.querySelector('.pp');
-
 window.addEventListener('scroll', function(){
-    var scrollY = window.scrollY;
-    var pos = sectionInner_fir.getBoundingClientRect();
-    var cx = scrollY * -0.5 - pos.top;
-    console.log(cx);
+    // 섹션01 - 1. 배경 이미지 움직임 - transform scale 일 때
+    var section01 = document.querySelector('.section01-inner');
+    var section01_test = window.pageYOffset / document.querySelector('.section01 h1').offsetTop + 1;
+    section01.style.transform = `scale(${section01_test})`;
 
-    sectionInner_fir.style.transform = 'translate3d(' + cx * 0.05 + '%, 0, 0)'
-    pp.style.transform = 'translate3d(' + cx * 0.5 + 'px, 0, 0)';
+    // 섹션02 - 2. 배경 이미지 움직임 - backgroundSize 일 때
+    var section02 = document.querySelector('.section02-inner');
+    var section02_test = window.pageYOffset/20;
+    section02.style.backgroundSize = 100 + section02_test + '%';
 
-    if(scrollY >= sectionInner_sec.offsetTop - window.pageYOffset){
-        sectionInner_sec.style.transform = 'translate3d(' + -cx * 0.05 + '%, 0, 0)';
+    // 섹션03 - 3. 이미지 확대
+    var section03 = document.querySelector('.section03');
+    var section03_inner = document.querySelector('.section03-inner');
+    if(window.scrollY >= section03_inner.getBoundingClientRect().top){
+        section03_inner.classList.add('section03_test');
     }else{
-        sectionInner_sec.style.transform = 'translate3d(0, 0, 0)';
+        section03_inner.classList.remove('section03_test');
     }
+    var section03_img = document.querySelectorAll('.section03_test .img-box img');  
+    for(var i=0; i<section03_img.length; i++){
+        section03_img[i].style.transform = `scale(${window.pageYOffset / section03.offsetTop})`
+    }
+
 });
+
+
+
+
+
